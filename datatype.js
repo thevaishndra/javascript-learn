64c98550b63eeb42b7de1c6bc545a899891c4299
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-04 21:04:03 +0530

    Create datatype.js

---
+++ datatype.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer  version
+// alert(6+6); // we are using nodejs not browser
+//code readability should be high
+
+let name = "Vaishnavi";
+let age = "20";
+let isLoggedIn = false;
+let state = null;
+// number => 2 to power 53
+// bigint => not used that much
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => empty container
+// symbol => used more in react
+console.log(typeof object);
