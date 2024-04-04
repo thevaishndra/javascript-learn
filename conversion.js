7d3233ac46e68e49bc9068dccb42eaefa4da3b5d
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-04 23:18:12 +0530

    Update conversion.js

--- conversion.js
+++ conversion.js
@@ -16,3 +16,20 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//******************operations***********************
+ let value = 3;
+ let negValue = -value;
+//  console.log(negValue);
+
+let str1 = "hello";
+let str2 = "Vaishnavi";
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3 + 4) * 5 % 3);
+
