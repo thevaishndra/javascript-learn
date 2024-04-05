fc33ce79b62cea6ae68d745e7270fffa7c79636f
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-05 11:34:59 +0530

    Update conversion.js

--- conversion.js
+++ conversion.js
@@ -5,17 +5,17 @@ let score = 33;// if it was null then output is 0
 console.log(typeof score);
 console.log(typeof (score));
 let valueInString = String(score);
-console.log(typeof valueInString);
+// console.log(typeof valueInString);
 
 let isLoggedIn = 1;
 // any value => true; empty"" => false
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 //******************operations***********************
  let value = 3;
@@ -27,15 +27,22 @@ let str2 = "Vaishnavi";
 let str3 = str1 + str2;
 // console.log(str3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
-console.log((3 + 4) * 5 % 3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
 
 //primitive {copy the value and do changes in copy}
 //7 types -> string, no, boolean, null, undefined, symbol, bigInt
 
 //reference {non primitive}
 //arrays, objects, functions
+
+//stack memory -> primitive, heap memory ->non primitive
+
+let name = "Vaishnavi";
+console.log(`Hello, I am ${name}`);//modern way of writing
+
+
 
