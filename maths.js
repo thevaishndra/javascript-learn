0d433a80537236c99b37757548eefb32645acea8
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-06 10:44:49 +0530

    Create maths.js

---
+++ maths.js
@@ -0,0 +1,27 @@
+balance = 1234.567;
+// console.log(balance.toFixed(1));
+// console.log(balance.toPrecision(1));
+
+hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math);
+// console.log(Math.abs(.4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 1, 9));
+// console.log(Math.max(4, 3, 1, 9));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10)+1);
+//It depends by which we are multiplying to get specific range
+// console.log((Math.floor(Math.random()*10)+1));//to get more specific lowest round off value
+
+const min = 10;
+const max =20;
+console.log((Math.random()*(max-min +1)+min));
+//for a specific range value
+
+
