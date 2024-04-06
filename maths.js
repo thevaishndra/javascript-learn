fc28180a2dbfe478d31a4303b018357763bc9711
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-06 17:05:17 +0530

    Update maths.js

--- maths.js
+++ maths.js
@@ -26,12 +26,27 @@ const max =20;
 
 // ********************* date and time ***********************
 let myDate = new Date();
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+// console.log(myDate.toString());//date, day, time, gmt ist 16: 49: 46 GMT+0530
+// console.log(myDate.toDateString());//date, day-> Sat Apr 06 2024
+// console.log(myDate.toLocaleString());// date-> 6/4/24, time 4:51:19pm
+// console.log(typeof myDate);//obj
 
-let myCreatedDate = new Date(2023, 0, 23);
-console.log(myCreatedDate.toDateString);
+let myCreatedDate = new Date(2023, 0, 23);//format of writing date
+// console.log(myCreatedDate.toDateString());
 
+let date = new Date("06-04-2024");//format of writing date
+// console.log(date.toLocaleDateString());
+
+let myTimeStamp = Date.now();
+//console.log(myTimeStamp);
+//console.log(date.getTime());
+///floor value for our random
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+newDate.toLocaleString('default',{
+    weekday: "long"// we can also add time zone
+});
 
