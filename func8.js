1185a051bab666bae8dd13412e9587ae2b47ddbe
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-10 20:25:54 +0530

    Create func8.js

---
+++ func8.js
@@ -0,0 +1,11 @@
+function sayMyName (){
+    console.log("Vaishnavi");
+}
+// sayMyName();
+function addTwoNumbers (number1, number2){
+    let result = number1 + number2;//parameters
+    return result;
+}
+// addTwoNumbers(3, 4);//arguments
+const result = addTwoNumbers(3,5);
+console.log("Result : ", result);
