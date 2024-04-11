251e369c860cd42bf500b80bb190af11bce1d861
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-11 13:12:44 +0530

    Update scope9.js

--- scope9.js
+++ scope9.js
@@ -1,9 +1,9 @@
-let a = 10;
-const b = 20;
-var c = 30;
+var c = 300;
 if (true){//scope
-
+    // let a = 10;//console.log will run inside not outside of scope
+    // const b = 20;//console.log will run inside not outside of scope
+    var c = 30;//problem in var -> scope
 }
-console.log(a);
-console.log(b);
-console.log(c);
+console.log(a);//not run outside of scope
+console.log(b);//not run outside of scope
+console.log(c);//it runs even if its outside of scope, when redeclared changes the value of c
