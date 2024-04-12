1079a15fb12fdcdae637d1119d2c67a9993eca6f
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-12 16:28:32 +0530

    Update this10.js

--- this10.js
+++ this10.js
@@ -12,3 +12,31 @@ const user = {
 // user.username = "shubh";
 // user.welcomeMessage();
 console.log(this);//refers to an empty object -> {}
+
+
+
+//******************arrow func **********************
+ const addTwo = (num1, num2) => {//curly braces ther is return keyword
+     return num1 + num2;//explixit return => when return keyword is used
+ }
+// console.log(addTwo(4, 5));
+ const addTwoo = (num1, num2) => num1 + num2;//implicit return but runs smoothly
+//parenthesis no return keyword
+// console.log(addTwoo(4, 5));
+//({username: "vaish"}) => to return object we write like this
+
+
+
+//IIFE => immediately invoked function expression
+//function that gets execute immediately, to remove and prevent pollution from global scope, we use iife
+
+//named iife as func has namme
+(function silver(){//refer line 25
+    console.log(`Database connected`);//it was throwing error first but then we wrote it inside parenthesis and made it a block
+})();//to end code we explicitly add semicolon, otherwise next code will not run
+
+
+//unnamed iife as func dont have a name
+((name)=>{
+    console.log(`DB connected ${name}`);
+})('vaish');//how to pass variable
