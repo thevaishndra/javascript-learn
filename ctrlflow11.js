984a0de924d1dbb7b1a631ad7d9660288cc2abb5
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-12 17:52:12 +0530

    Create ctrlflow11.js

---
+++ ctrlflow11.js
@@ -0,0 +1,7 @@
+//if
+const isUserLoggedIn = true
+if(2 == "2"){//here  one is number and one is string 
+console.log("executed");//error as they are !=
+}
+// <, >, <=, >=, ==, !=, ===
+//= operator assignment
