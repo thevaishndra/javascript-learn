89621a47d542cb66c72b46afd5b51336db6fb622
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-13 20:58:58 +0530

    Update ctrlflow11.js

--- ctrlflow11.js
+++ ctrlflow11.js
@@ -41,6 +41,36 @@ const userLoggedIn = true;
 const debitCard = true;
 const loggedInfromGoogle = true;
 const loggedInfromEmail = false;
-if (userLoggedIn && debitCard && 2==3 ){
-    console.log("Allowed to spend money");
+// if (userLoggedIn && debitCard && 2==3 ){
+//     console.log("Allowed to spend money");
+// }
+
+
+//switch
+const day = 6;
+switch (day) {
+    case 1:
+        console.log("mon");
+        break;
+    case 2:
+        console.log("tue");
+        break;
+    case 3:
+        console.log("wed");
+        break;
+    case 4:
+        console.log("thu");
+        break;
+    case 5:
+        console.log("fri");
+        break;
+    case 6:
+        console.log("sat");
+        break;
+    case 7:
+        console.log("sun");
+        break;
+    default:
+        console.log("enter a valid day")
+        break;
 }
