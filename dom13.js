e713f5bd9451bef8f82f14d6a2dd753505da8794
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-04-17 23:58:01 +0530

    dom selectors

---
+++ dom13.js
@@ -0,0 +1,25 @@
+//document.getElementById('');
+//document.getElementById('').getAttribute('');
+//title = document.getElementById('');
+//title.style.backgroundColor = 'green';
+//title.style.padding = '15px';
+//title.style.borderRadius  = '5px';
+
+//title.textContent =>hidden text is also shown
+//title.innerText => visible text on screen
+//title.innerHTML => shows html code
+
+//document.querySelector('#title'); =>for id
+//document.querySelector('.heading'); =>for class
+
+//document.querySelector('ul');
+//const myul = document.querySelector('ul');
+//myul.querySelector('li');
+//const turngreen = myul.querySelector('li');
+//turngreen.style.backgroundColor = "green";
+
+//document.querySelectorAll('li');//they're not pure array, in array prototype they have map
+//tempLiList[0].style.color = 'green';//node list => square brackets
+//tempLiList.forEach( function(l) {
+//  length.style.backgroundColor = "green"
+//})
