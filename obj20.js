ab22929fd1ed1e849e843fb7e17b531b6a7c3862
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-06-12 20:26:26 +0530

    Update obj20.js

--- obj20.js
+++ obj20.js
@@ -53,6 +53,10 @@ Array.prototype.heyVaish = function(){
 // charSpecial.heyVaish()
 
 //INHERITANCE
+const user = {
+    name: "chai",
+    email: "chai@123"
+}
 const Teacher = {
     makeVideo: true
 }
@@ -62,5 +66,38 @@ const TeacherSupport = {
 const TASupport = {
     makeAssignment : 'JS Assignment',
     fullTime: true,
-    __proto__
+    __proto__: TeacherSupport
 }
+
+Teacher.__proto__ = user//how we can access someone else's properties
+
+//modern syntax
+Object.setPrototypeOf(TeacherSupport, Teacher)
+
+let anotherUsername = "IAmTheBest"
+String.prototype.trueLength =  function(){
+    console.log(`${this}`);
+    console.log(`${this.name}`);
+    console.log(`True Length is: ${this.trim().length}`);
+}
+
+// anotherUsername.trueLength()
+// "vaishnavi".trueLength()
+// "sunscreen".trueLength()
+
+
+
+//CALL AND THIS
+function setUsername (username){
+    //complex db calls
+    this.username = username//this method isn't getting called
+}
+function createUser(username, email, password){
+    setUsername(username)//reference is done it is not called
+    setUsername.call(this, username)//now, it get's technically called and it also holds the reference. sp, that it doesnt get erased frpm the memory
+    //it is called but nothing is changed in final so we use this,
+    this.email = email
+    this.password = password
+}
+const coffee = new createUser ("coffee", "coffee@1", "123")
+console.log(coffee);
