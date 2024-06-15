2ee8ae143e67c225640a95370445625cc58b985a
thevaishndra <124268459+thevaishndra@users.noreply.github.com>
2024-06-15 12:59:45 +0530

    Update mathpi23.js

--- mathpi23.js
+++ mathpi23.js
@@ -7,12 +7,22 @@ console.log(descriptor);
 const cake = {
     name: 'red velvet',
     price: '600',
-    isAvailable: true
+    isAvailable: true,
+
+    orderCake: function(){
+        console.log("cake not made");
+    }
 }
 console.log(Object.getOwnPropertyDescriptor(cake, "name"));
 
 Object.defineProperty(cake, 'name', {
-    writable: false,
-    enumerable: false
+    writable: false,//comment this
+    enumerable: false//and change value, the iteration will be different
 })
 console.log(Object.getOwnPropertyDescriptor(cake, "name"));
+
+for (let [key, value] of Object.entries(cake)) {//now cake is iterable if we add object.entries
+    if (typeof value !== 'function'){
+        console.log(`${key} : ${value}`);
+    }
+}
